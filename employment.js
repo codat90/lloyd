275013ac5d1c04ede67345f89688b8b0af594b1b
codat90 <40149299+codat90@users.noreply.github.com>
2021-06-22 02:00:14 +0300

    Add files via upload

---
+++ employment.js
@@ -0,0 +1,12 @@
+function changeText(text)
+{
+	var display = document.getElementById('text-display');
+	display.innerHTML = "";
+	display.innerHTML = text;
+}
+  function changeback(text)
+{
+	var display = document.getElementById('text-display');
+	display.innerHTML = "";
+	display.innerHTML = text;
+}
